ec42d369752659b17b38f43cd7952dc12cf2fc2b
mariioka <125134709+mariioka@users.noreply.github.com>
2025-03-20 11:10:44 -0300

    Create PokedexView.swift

---
+++ Projeto_Pokedex/Projeto_Pokedex/view/PokedexView.swift
@@ -0,0 +1,62 @@
+//
+//  PokedexView.swift
+//  foundation_pokedex
+//
+//  Created by Diego Saragoza Da Silva on 18/03/25.
+//
+
+import SwiftUI
+
+struct PokedexView : View {
+    var body : some View {
+        VStack{
+            Text("gg")
+            ZStack{
+
+//            Rectangle()
+//                .fill(.yellow)
+//                .frame(width:340, height: 190)
+//                VStack{
+//                    Text("id: 1")
+//                    Text("Nome : bulbasaur")
+//                    Text("types: [.grass, .poison]")
+                    
+                    
+                }
+            
+            }
+//            Rectangle()
+//                .fill(.green)
+//                .frame(width:340, height: 190)
+//            Rectangle()
+//                .fill(.red)
+//                .frame(width:340, height: 190)
+//            Rectangle()
+//                .fill(.blue)
+//                .frame(width:340, height: 190)
+//            NavigationStack{
+//                Text("POKEDEX@@@")
+//                    .navigationTitle("POKEDEX!!!")
+//                    .navigationBarTitleDisplayMode(.inline)
+//                    .fontWeight(.semibold)
+//                    .font(.title)
+//                    .foregroundColor(.green)
+            }
+//                VStack(){
+//                    Text("POKEDEX@@@")
+//                    Text("POKEDEXyyy")
+//                        .fontWeight(.semibold)
+//                        .font(.title)
+//                        .foregroundColor(.green)
+//                }
+
+        }
+ }
+
+//}
+
+struct PokedexView_Previews: PreviewProvider {
+    static var previews: some View {
+        PokedexView()
+    }
+}
