6a4165f36853cfe819af6fbc86ba6741d4dc22ec
mariioka <125134709+mariioka@users.noreply.github.com>
2025-03-20 11:49:35 -0300

    Update image.swift

--- mCodigo/image.swift
+++ mCodigo/image.swift
@@ -8,3 +8,39 @@ struct Pokemon {
         String(format: "%@%03d.webp", name, id)
     }
 }
+
+
+import SwiftUI
+
+struct PokemonCardView: View {
+    let pokemon: Pokemon
+
+    var body: some View {
+        VStack(alignment: .leading) {
+            Image(pokemon.imageName) // Exibe a imagem
+                .resizable()
+                .scaledToFit()
+                .frame(height: 120)
+                .background(Color.gray.opacity(0.2))
+                .cornerRadius(8)
+
+            Text(pokemon.name.capitalized)
+                .font(.headline)
+                .padding(.top, 4)
+
+            HStack {
+                ForEach(pokemon.types, id: \.self) { type in
+                    Text(type.rawValue.capitalized)
+                        .font(.caption)
+                        .padding(5)
+                        .background(Color.blue.opacity(0.2))
+                        .cornerRadius(5)
+                }
+            }
+        }
+        .padding()
+        .background(Color.white)
+        .cornerRadius(12)
+        .shadow(radius: 4)
+    }
+}
